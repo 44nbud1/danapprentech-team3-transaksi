0e858e2c8e72785a0b102b30d75dd2772819371a
Aan Budi Setiawan <setiawanaa@dnid0695l01.local>
2020-05-14 00:05:19 +0700

    switch

--- src/main/java/project/akhir/danapprentechteam3/transaksi/service/CustomerChoiceService.java
+++ src/main/java/project/akhir/danapprentechteam3/transaksi/service/CustomerChoiceService.java
@@ -71,52 +71,322 @@ public class CustomerChoiceService
                 switch (customerChoice.getId()){
                     case 1 :
                         choice.setHarga(27000L);
-                        choice.setNamaProvider("indosat");
+                        choice.setNamaProvider("simpati");
                         choice.setPaketData("Paket-OMG-1GB");
                         break;
                     case 2 :
                         choice.setHarga(32000L);
-                        choice.setNamaProvider("indosat");
+                        choice.setNamaProvider("simpati");
                         choice.setPaketData("Paket-OMG-2GB");
                         break;
                     case 3 :
                         choice.setHarga(39000L);
-                        choice.setNamaProvider("indosat");
+                        choice.setNamaProvider("simpati");
                         choice.setPaketData("Paket-OMG-3GB");
                         break;
                     case 4 :
                         choice.setHarga(63000L );
-                        choice.setNamaProvider("indosat");
+                        choice.setNamaProvider("simpati");
                         choice.setPaketData("Paket-OMG-4GB");
                         break;
                     case 5 :
                         choice.setHarga(89000L);
-                        choice.setNamaProvider("indosat");
+                        choice.setNamaProvider("simpati");
                         choice.setPaketData("Paket-OMG-6GB");
                         break;
                     case 6 :
                         choice.setHarga(25000L);
-                        choice.setNamaProvider("indosat");
+                        choice.setNamaProvider("simpati");
                         choice.setPaketData("Freedom-Combo-4GB");
                         break;
                     case 7 :
                         choice.setHarga(35000L);
-                        choice.setNamaProvider("indosat");
+                        choice.setNamaProvider("simpati");
                         choice.setPaketData("Freedom-Combo-6GB");
                         break;
                     case 8 :
                         choice.setHarga(45000L);
-                        choice.setNamaProvider("indosat");
+                        choice.setNamaProvider("simpati");
                         choice.setPaketData("Freedom-Combo-8GB");
                         break;
                     case 9 :
                         choice.setHarga(65000L);
-                        choice.setNamaProvider("indosat");
+                        choice.setNamaProvider("simpati");
                         choice.setPaketData("Freedom-Combo-10GB");
                         break;
                     case 10 :
                         choice.setHarga(80000L);
-                        choice.setNamaProvider("indosat");
+                        choice.setNamaProvider("simpati");
+                        choice.setPaketData("Freedom-Combo-12GB");
+                        break;
+                }
+
+            case "xl" :
+                switch (customerChoice.getId()){
+                    case 1 :
+                        choice.setHarga(27000L);
+                        choice.setNamaProvider("xl");
+                        choice.setPaketData("Paket-OMG-1GB");
+                        break;
+                    case 2 :
+                        choice.setHarga(32000L);
+                        choice.setNamaProvider("xl");
+                        choice.setPaketData("Paket-OMG-2GB");
+                        break;
+                    case 3 :
+                        choice.setHarga(39000L);
+                        choice.setNamaProvider("xl");
+                        choice.setPaketData("Paket-OMG-3GB");
+                        break;
+                    case 4 :
+                        choice.setHarga(63000L );
+                        choice.setNamaProvider("xl");
+                        choice.setPaketData("Paket-OMG-4GB");
+                        break;
+                    case 5 :
+                        choice.setHarga(89000L);
+                        choice.setNamaProvider("xl");
+                        choice.setPaketData("Paket-OMG-6GB");
+                        break;
+                    case 6 :
+                        choice.setHarga(25000L);
+                        choice.setNamaProvider("xl");
+                        choice.setPaketData("Freedom-Combo-4GB");
+                        break;
+                    case 7 :
+                        choice.setHarga(35000L);
+                        choice.setNamaProvider("xl");
+                        choice.setPaketData("Freedom-Combo-6GB");
+                        break;
+                    case 8 :
+                        choice.setHarga(45000L);
+                        choice.setNamaProvider("xl");
+                        choice.setPaketData("Freedom-Combo-8GB");
+                        break;
+                    case 9 :
+                        choice.setHarga(65000L);
+                        choice.setNamaProvider("xl");
+                        choice.setPaketData("Freedom-Combo-10GB");
+                        break;
+                    case 10 :
+                        choice.setHarga(80000L);
+                        choice.setNamaProvider("xl");
+                        choice.setPaketData("Freedom-Combo-12GB");
+                        break;
+                }
+
+            case "as" :
+                switch (customerChoice.getId()){
+                    case 1 :
+                        choice.setHarga(27000L);
+                        choice.setNamaProvider("as");
+                        choice.setPaketData("Paket-OMG-1GB");
+                        break;
+                    case 2 :
+                        choice.setHarga(32000L);
+                        choice.setNamaProvider("as");
+                        choice.setPaketData("Paket-OMG-2GB");
+                        break;
+                    case 3 :
+                        choice.setHarga(39000L);
+                        choice.setNamaProvider("as");
+                        choice.setPaketData("Paket-OMG-3GB");
+                        break;
+                    case 4 :
+                        choice.setHarga(63000L );
+                        choice.setNamaProvider("as");
+                        choice.setPaketData("Paket-OMG-4GB");
+                        break;
+                    case 5 :
+                        choice.setHarga(89000L);
+                        choice.setNamaProvider("as");
+                        choice.setPaketData("Paket-OMG-6GB");
+                        break;
+                    case 6 :
+                        choice.setHarga(25000L);
+                        choice.setNamaProvider("as");
+                        choice.setPaketData("Freedom-Combo-4GB");
+                        break;
+                    case 7 :
+                        choice.setHarga(35000L);
+                        choice.setNamaProvider("as");
+                        choice.setPaketData("Freedom-Combo-6GB");
+                        break;
+                    case 8 :
+                        choice.setHarga(45000L);
+                        choice.setNamaProvider("as");
+                        choice.setPaketData("Freedom-Combo-8GB");
+                        break;
+                    case 9 :
+                        choice.setHarga(65000L);
+                        choice.setNamaProvider("as");
+                        choice.setPaketData("Freedom-Combo-10GB");
+                        break;
+                    case 10 :
+                        choice.setHarga(80000L);
+                        choice.setNamaProvider("as");
+                        choice.setPaketData("Freedom-Combo-12GB");
+                        break;
+                }
+
+            case "three" :
+                switch (customerChoice.getId()){
+                    case 1 :
+                        choice.setHarga(27000L);
+                        choice.setNamaProvider("three");
+                        choice.setPaketData("Paket-OMG-1GB");
+                        break;
+                    case 2 :
+                        choice.setHarga(32000L);
+                        choice.setNamaProvider("three");
+                        choice.setPaketData("Paket-OMG-2GB");
+                        break;
+                    case 3 :
+                        choice.setHarga(39000L);
+                        choice.setNamaProvider("three");
+                        choice.setPaketData("Paket-OMG-3GB");
+                        break;
+                    case 4 :
+                        choice.setHarga(63000L );
+                        choice.setNamaProvider("three");
+                        choice.setPaketData("Paket-OMG-4GB");
+                        break;
+                    case 5 :
+                        choice.setHarga(89000L);
+                        choice.setNamaProvider("three");
+                        choice.setPaketData("Paket-OMG-6GB");
+                        break;
+                    case 6 :
+                        choice.setHarga(25000L);
+                        choice.setNamaProvider("three");
+                        choice.setPaketData("Freedom-Combo-4GB");
+                        break;
+                    case 7 :
+                        choice.setHarga(35000L);
+                        choice.setNamaProvider("three");
+                        choice.setPaketData("Freedom-Combo-6GB");
+                        break;
+                    case 8 :
+                        choice.setHarga(45000L);
+                        choice.setNamaProvider("three");
+                        choice.setPaketData("Freedom-Combo-8GB");
+                        break;
+                    case 9 :
+                        choice.setHarga(65000L);
+                        choice.setNamaProvider("three");
+                        choice.setPaketData("Freedom-Combo-10GB");
+                        break;
+                    case 10 :
+                        choice.setHarga(80000L);
+                        choice.setNamaProvider("three");
+                        choice.setPaketData("Freedom-Combo-12GB");
+                        break;
+                }
+
+            case "smartfren" :
+                switch (customerChoice.getId()){
+                    case 1 :
+                        choice.setHarga(27000L);
+                        choice.setNamaProvider("smartfren");
+                        choice.setPaketData("Paket-OMG-1GB");
+                        break;
+                    case 2 :
+                        choice.setHarga(32000L);
+                        choice.setNamaProvider("smartfren");
+                        choice.setPaketData("Paket-OMG-2GB");
+                        break;
+                    case 3 :
+                        choice.setHarga(39000L);
+                        choice.setNamaProvider("smartfren");
+                        choice.setPaketData("Paket-OMG-3GB");
+                        break;
+                    case 4 :
+                        choice.setHarga(63000L );
+                        choice.setNamaProvider("smartfren");
+                        choice.setPaketData("Paket-OMG-4GB");
+                        break;
+                    case 5 :
+                        choice.setHarga(89000L);
+                        choice.setNamaProvider("smartfren");
+                        choice.setPaketData("Paket-OMG-6GB");
+                        break;
+                    case 6 :
+                        choice.setHarga(25000L);
+                        choice.setNamaProvider("smartfren");
+                        choice.setPaketData("Freedom-Combo-4GB");
+                        break;
+                    case 7 :
+                        choice.setHarga(35000L);
+                        choice.setNamaProvider("smartfren");
+                        choice.setPaketData("Freedom-Combo-6GB");
+                        break;
+                    case 8 :
+                        choice.setHarga(45000L);
+                        choice.setNamaProvider("smartfren");
+                        choice.setPaketData("Freedom-Combo-8GB");
+                        break;
+                    case 9 :
+                        choice.setHarga(65000L);
+                        choice.setNamaProvider("smartfren");
+                        choice.setPaketData("Freedom-Combo-10GB");
+                        break;
+                    case 10 :
+                        choice.setHarga(80000L);
+                        choice.setNamaProvider("smartfren");
+                        choice.setPaketData("Freedom-Combo-12GB");
+                        break;
+                }
+
+            case "axis" :
+                switch (customerChoice.getId()){
+                    case 1 :
+                        choice.setHarga(27000L);
+                        choice.setNamaProvider("axis");
+                        choice.setPaketData("Paket-OMG-1GB");
+                        break;
+                    case 2 :
+                        choice.setHarga(32000L);
+                        choice.setNamaProvider("axis");
+                        choice.setPaketData("Paket-OMG-2GB");
+                        break;
+                    case 3 :
+                        choice.setHarga(39000L);
+                        choice.setNamaProvider("axis");
+                        choice.setPaketData("Paket-OMG-3GB");
+                        break;
+                    case 4 :
+                        choice.setHarga(63000L );
+                        choice.setNamaProvider("axis");
+                        choice.setPaketData("Paket-OMG-4GB");
+                        break;
+                    case 5 :
+                        choice.setHarga(89000L);
+                        choice.setNamaProvider("axis");
+                        choice.setPaketData("Paket-OMG-6GB");
+                        break;
+                    case 6 :
+                        choice.setHarga(25000L);
+                        choice.setNamaProvider("axis");
+                        choice.setPaketData("Freedom-Combo-4GB");
+                        break;
+                    case 7 :
+                        choice.setHarga(35000L);
+                        choice.setNamaProvider("axis");
+                        choice.setPaketData("Freedom-Combo-6GB");
+                        break;
+                    case 8 :
+                        choice.setHarga(45000L);
+                        choice.setNamaProvider("axis");
+                        choice.setPaketData("Freedom-Combo-8GB");
+                        break;
+                    case 9 :
+                        choice.setHarga(65000L);
+                        choice.setNamaProvider("axis");
+                        choice.setPaketData("Freedom-Combo-10GB");
+                        break;
+                    case 10 :
+                        choice.setHarga(80000L);
+                        choice.setNamaProvider("axis");
                         choice.setPaketData("Freedom-Combo-12GB");
                         break;
                 }
