1cb286dd411050277198018581211b7a4329685f
Aan Budi Setiawan <setiawanaa@dnid0695l01.local>
2020-05-14 12:11:20 +0700

    delete rabbit

--- src/main/java/project/akhir/danapprentechteam3/transaksi/rabbitMQ/consumer/TransaksiMessageListener.java
+++ src/main/java/project/akhir/danapprentechteam3/transaksi/rabbitMQ/consumer/TransaksiMessageListener.java
@@ -1,27 +1,27 @@
-package project.akhir.danapprentechteam3.transaksi.rabbitMQ.consumer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
-import project.akhir.danapprentechteam3.transaksi.model.Transaksi;
-import project.akhir.danapprentechteam3.transaksi.rabbitMQ.config.RabbitConfig;
-
-@Component
-public class TransaksiMessageListener {
-    static final Logger logger = LoggerFactory.getLogger(TransaksiMessageListener.class);
-    private Transaksi transaksi;
-
-    @RabbitListener(queues = RabbitConfig.QUEUE_ORDERS)
-    public void processOrder(Transaksi transaksi) {
-        logger.info("Transaksi Received: "+transaksi);
-        this.transaksi = transaksi;
-    }
-
-
-    public Transaksi getTransaksi() {
-        return transaksi;
-    }
-
-
-}
+//package project.akhir.danapprentechteam3.transaksi.rabbitMQ.consumer;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Component;
+//import project.akhir.danapprentechteam3.transaksi.model.Transaksi;
+//import project.akhir.danapprentechteam3.transaksi.rabbitMQ.config.RabbitConfig;
+//
+//@Component
+//public class TransaksiMessageListener {
+//    static final Logger logger = LoggerFactory.getLogger(TransaksiMessageListener.class);
+//    private Transaksi transaksi;
+//
+//    @RabbitListener(queues = RabbitConfig.QUEUE_ORDERS)
+//    public void processOrder(Transaksi transaksi) {
+//        logger.info("Transaksi Received: "+transaksi);
+//        this.transaksi = transaksi;
+//    }
+//
+//
+//    public Transaksi getTransaksi() {
+//        return transaksi;
+//    }
+//
+//
+//}
--- src/main/java/project/akhir/danapprentechteam3/transaksi/rabbitMQ/producer/TransaksiMessageSender.java
+++ src/main/java/project/akhir/danapprentechteam3/transaksi/rabbitMQ/producer/TransaksiMessageSender.java
@@ -1,24 +1,24 @@
-package project.akhir.danapprentechteam3.transaksi.rabbitMQ.producer;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import project.akhir.danapprentechteam3.transaksi.model.Transaksi;
-import project.akhir.danapprentechteam3.transaksi.rabbitMQ.config.RabbitConfig;
-
-@Service
-public class TransaksiMessageSender {
-    private final RabbitTemplate rabbitTemplate;
-    private final ObjectMapper objectMapper;
-
-    @Autowired
-    public TransaksiMessageSender(RabbitTemplate rabbitTemplate, ObjectMapper objectMapper) {
-        this.rabbitTemplate = rabbitTemplate;
-        this.objectMapper = objectMapper;
-    }
-
-    public void sendOrder(Transaksi transaksi) {
-        this.rabbitTemplate.convertAndSend(RabbitConfig.QUEUE_ORDERS, transaksi);
-    }
-}
+//package project.akhir.danapprentechteam3.transaksi.rabbitMQ.producer;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import project.akhir.danapprentechteam3.transaksi.model.Transaksi;
+//import project.akhir.danapprentechteam3.transaksi.rabbitMQ.config.RabbitConfig;
+//
+//@Service
+//public class TransaksiMessageSender {
+//    private final RabbitTemplate rabbitTemplate;
+//    private final ObjectMapper objectMapper;
+//
+//    @Autowired
+//    public TransaksiMessageSender(RabbitTemplate rabbitTemplate, ObjectMapper objectMapper) {
+//        this.rabbitTemplate = rabbitTemplate;
+//        this.objectMapper = objectMapper;
+//    }
+//
+//    public void sendOrder(Transaksi transaksi) {
+//        this.rabbitTemplate.convertAndSend(RabbitConfig.QUEUE_ORDERS, transaksi);
+//    }
+//}
